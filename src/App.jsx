6f73e3ca232d2c6fb8aa3c0c5d734f8f44a53b1d
zori13 <zori.gon@mail.com>
2024-03-28 16:50:59 +0100

    Actualizar componente App

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {AppLayout} from './components/ui/layuouts'
 function App() {
   return(
     <AppLayout>
-       <h1 className="bg-green-300 font-bold text-2xl underline">ZoriPro Prestamos DeFi</h1> 
+       <h1 className="underline">ZoriPro</h1> 
     </AppLayout>    
   )
 }
