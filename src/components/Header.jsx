4602627b597e46df94881fbceaaef24421c84a62
zori13 <zori.gon@mail.com>
2024-03-28 16:43:08 +0100

    Poner el Header responsivo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 
 export default function Header(){
     return (
-    <header className="bg-green-400 text-3xl underline py-2"> 
+    <header className="border-b shadow-sm py-2  flex justify-between items-center" > 
         <img src="./public/billetes-grande.png" alt="ZoriPro-mobile-logo" className="sm:hidden" width={45}/>
         <img src="./public/billetes-grande.png" alt="ZoriPro-desktop-logo" className="hidden sm:block" width={120}/>
+        <button className="bg-pink-300 rounded-xl px-2 py-2 text-s h-fit">Connect Wallet</button>
     </header>
     )
 }
