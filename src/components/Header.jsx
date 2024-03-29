4c19b3c6aee047175a32df935d5c49a91befc16f
zori13 <zori.gon@mail.com>
2024-03-29 17:07:32 +0100

    Añadir el botón de conexión de connectkit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 
+import { ConnectKitButton } from "connectkit"
 export default function Header(){
     return (
     <header className="border-b shadow-sm py-2  flex justify-between items-center" > 
         <img src="./public/billetes-grande.png" alt="ZoriPro-mobile-logo" className="sm:hidden" width={45}/>
         <img src="./public/billetes-grande.png" alt="ZoriPro-desktop-logo" className="hidden sm:block" width={120}/>
-        <button className="bg-pink-300 rounded-xl px-2 py-2 text-s h-fit">Connect Wallet</button>
+        <ConnectKitButton />
     </header>
     )
 }
