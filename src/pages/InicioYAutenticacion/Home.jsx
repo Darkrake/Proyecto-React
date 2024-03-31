5aec31012398ed68b65106a3c27e42b1c88d4897
zori13 <zori.gon@mail.com>
2024-03-31 22:33:56 +0200

    Crear la estructura de los componentes para interactuar con el contrato

--- src/pages/InicioYAutenticacion/Home.jsx
+++ src/pages/InicioYAutenticacion/Home.jsx
@@ -1,7 +1,16 @@
+import { useAccount } from "wagmi"
+import { IndicadorCarga } from "../../components/ui";
+
 export default function Home(){
+    const { address, isConnecting, isDisconnected } = useAccount();
+    if (isConnecting) return <div>Connecting...</div>
+    if (isDisconnected) return <div>Disconnected</div>
+    
     return(
         <div>
-            <h1 className="underline">ZoriPro</h1> 
+            <div className="flex flex-col">Connected Wallet: {address}</div>
+            <IndicadorCarga className="h-10 w-10" />
         </div>
     )
+
 }
