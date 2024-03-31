860ad16c346ab5f96bb27cb8a280c0b7ae59cd68
zori13 <zori.gon@mail.com>
2024-03-31 22:33:11 +0200

    Actualizar el index de ui

---
+++ src/components/ui/index.js
@@ -0,0 +1,5 @@
+export{default as IndicadorCarga} from './IndicadorCarga'
+//export{default as Boton} from './Boton'
+//export{default as TextInput} from './TextInput'
+//export{default as Titulo} from './Titulo'
+//export{default as Error} from './Error'
