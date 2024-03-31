aa23af43c276cec1dbf968b3eb5a5ee7858a258a
zori13 <zori.gon@mail.com>
2024-03-31 22:08:05 +0200

    Implementar el boton para conectar a la wallet con connectkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import {AppLayout} from './components/ui/layuouts'
-import {Home} from './pages'
+import { AppLayout } from './components/ui/layuouts'
+import { Home } from './pages'
+import { WagmiConfig } from "wagmi"
+import { ConnectKitProvider } from "connectkit"
+import { config } from './components/config/wagmi'
+
+
 
 function App() {
+  
   return(
-    <AppLayout>
-       <Home/> 
-    </AppLayout>    
+    <WagmiConfig config={config}>
+      <ConnectKitProvider>
+        <AppLayout>
+          <Home/> 
+        </AppLayout>   
+      </ConnectKitProvider>
+    </WagmiConfig> 
   )
 }
 
