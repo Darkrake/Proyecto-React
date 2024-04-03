559df4f198f134d49db1ae95192b1698f00d0a65
zori13 <zori.gon@mail.com>
2024-04-03 22:09:07 +0200

    Actualizar AppLayout

--- src/components/ui/layuouts/AppLayout.jsx
+++ src/components/ui/layuouts/AppLayout.jsx
@@ -1,4 +1,4 @@
-import {Header, Footer} from "../../index";
+import {Header, Footer,Navbar} from "../../index"
 import PropTypes from 'prop-types'
 
 export default function AppLayout({children}){
@@ -6,6 +6,7 @@ export default function AppLayout({children}){
         <>
             <Header/>
                 <main className="min-h-[80vh]">{children}</main>
+                <div className="bg-gradient-to-r from-green-500 to-green-400 py-2 px-2 content-end border-b shadow-sm sm:hidden"><Navbar/></div>
             <Footer/>
         </>
     )
