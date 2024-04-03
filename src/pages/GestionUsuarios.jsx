4e8f62242e317063650c8000de8385be2c814af3
zori13 <zori.gon@mail.com>
2024-04-03 22:09:25 +0200

    Actualizar GestionUsuarios

---
+++ src/pages/GestionUsuarios.jsx
@@ -0,0 +1,20 @@
+import { AltaPrestamista, AltaCliente } from "../components"
+
+export default function GestionUsuarios() {
+  return (
+    <div>
+      <div className="text-5xl pt-10 font-thin text-center text-green-700">
+        <h2>Gestión de Usuarios</h2>
+        <hr />
+      </div>
+      <section className="flex flex-col pt-10 gap-16 items-center">
+        <div>
+          <AltaPrestamista />
+        </div>
+        <div>
+          <AltaCliente />
+        </div>
+      </section>
+    </div>
+  )
+}
