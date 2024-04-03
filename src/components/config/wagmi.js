07fa736107c5e551be0d3a01bc201a1a536a50a6
zori13 <zori.gon@mail.com>
2024-04-03 21:59:35 +0200

    Corrección configuración Wagmi

--- src/components/config/wagmi.js
+++ src/components/config/wagmi.js
@@ -1,15 +1,15 @@
 import { createConfig } from "wagmi"
 import { getDefaultConfig } from "connectkit"
-import {sepolia} from "wagmi/chains"
+import { sepolia } from "wagmi/chains"
 
 export const config = createConfig(
   getDefaultConfig({
-    
-    alchemyId: import.meta.env.VITE_ALCHEMY_ID, // or infuraId
+    alchemyId: import.meta.env.VITE_ALCHEMY_ID,
     walletConnectProjectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
 
-    // Required
-    appName: "ZoriPro-react",  
-    chains:[sepolia] 
+    appName: "ZoriPro-react",
+    chains: [sepolia],
+    
   })
 )
+export default config;
