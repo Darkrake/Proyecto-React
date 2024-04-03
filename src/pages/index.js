1159936f0cab0a221c98654bcd49bc588e3112af
zori13 <zori.gon@mail.com>
2024-04-03 22:05:35 +0200

    Actualizar index de pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,4 @@
-export {default as Home} from './InicioYAutenticacion/Home'
+export { default as Home } from "./Home";
+export { default as Autenticacion } from "./Autenticacion";
+export { default as GestionUsuarios } from "./GestionUsuarios";
+export { default as OperacionesPrestamos } from "./OperacionesPrestamos";
