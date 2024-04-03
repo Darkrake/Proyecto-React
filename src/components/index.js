41b445f06b7d1a88cd6debcc0c8450ad80e26755
zori13 <zori.gon@mail.com>
2024-04-03 22:03:35 +0200

    Actualizar el index de components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,11 @@
-export {default as Header} from './Header'
-export {default as Footer} from './Footer'
-//export {default as Navbar} from './NavBar'
+export { default as Header } from "./Header"
+export { default as Footer } from "./Footer"
+export { default as Navbar } from "./Navbar"
+export { default as AltaPrestamista } from "./AltaPrestamista"
+export { default as AltaCliente } from "./AltaCliente"
+export { default as SolicitarPrestamo } from "./SolicitarPrestamo"
+export { default as ReembolsarPrestamo } from "./ReembolsarPrestamo"
+export { default as PrestamosCliente } from "./PrestamosCliente"
+export { default as AprobarPrestamo } from "./AprobarPrestamo"
+export { default as LiquidarGarantia } from "./LiquidarGarantia"
+export { default as DepositarGarantia} from "./DepositarGarantia"
