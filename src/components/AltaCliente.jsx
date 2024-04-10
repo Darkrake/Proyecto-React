be6d9381b39223e94c3bdd90335588a60cd418e1
zori13 <zori.gon@mail.com>
2024-04-10 21:04:24 +0200

    Cambiar nombre de las variables

--- src/components/AltaCliente.jsx
+++ src/components/AltaCliente.jsx
@@ -5,20 +5,22 @@ import { useState, useEffect } from "react"
 import { toast } from 'react-hot-toast'
 
 export default function AltaCliente() {
-  const [nuevoCliente, setNuevoCliente] = useState("")
+  const [addressCliente, setAddressCliente] = useState("")
   const { address } = useAccount()
   const { data } = useContractRead({
     address: import.meta.env.VITE_ZoriProPrestamosDefi,
     abi: ABIZoriPro,
     functionName: "altaCliente",
-  })
+    })
+ 
   const isLenderEmployee = address === data
   const { config } = usePrepareContractWrite({
     address: import.meta.env.VITE_ZoriProPrestamosDefi,
     abi: ABIZoriPro,
     functionName: "altaCliente",
-    enabled: nuevoCliente,
-    args: [nuevoCliente],
+    enabled: addressCliente,
+    args: [addressCliente],
+   
   })
   const { data: writeData, write } = useContractWrite(config)
   const {
@@ -27,20 +29,23 @@ export default function AltaCliente() {
     isError: isTransactionError,
   } = useWaitForTransaction({
     hash: writeData?.hash,
+  
   })
-  const handleClientAddressInputChange = (event) => {
-    setNuevoCliente(event.target.value)
+  console.log({writeData})
+  const handleNuevoClienteInputChange = (event) => {
+    setAddressCliente(event.target.value)
   }
-
+  
   useEffect(() => {
     if (isTransactionSuccess) {
       toast.success("Cliente dado de alta con éxito")
-      setNuevoCliente("")
+      setAddressCliente("")
     }
     if (isTransactionError) {
       toast.error("Error: La transacción ha fallado")
     }
   }, [isTransactionSuccess, isTransactionError])
+  
   return (
     <section className="grid gap-3 px-5 bg-white p-4 border shadow rounded-lg text-sm w-fit">
       <Titulo>Alta Cliente</Titulo>
@@ -48,18 +53,19 @@ export default function AltaCliente() {
         <TextInput
           type="text"
           placeholder="Address Nuevo Cliente"
-          label="clientAddress"
-          value={nuevoCliente}
+          label="nuevoCliente"
+          value={addressCliente}          
           disabled={!isLenderEmployee || isTransactionLoading}
-          onChange={handleClientAddressInputChange}
+          onChange={handleNuevoClienteInputChange}
         />
+
       </form>
       <Boton
-        disabled={!nuevoCliente || !isLenderEmployee || isTransactionLoading}
+        disabled={!addressCliente || !isLenderEmployee || isTransactionLoading}
         isLoading={isTransactionLoading}
         onClick={() => write?.()}
       >
-        {isLenderEmployee
+          {isLenderEmployee
           ? isTransactionLoading
             ? "Tramitando Alta Nuevo Cliente"
             : "Alta Cliente"
