a53272c986bbe5fae3e44df096ff9d4fff70fdd7
zori13 <zori.gon@mail.com>
2024-04-10 21:03:27 +0200

    Añadir Toaster

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import { WagmiConfig } from "wagmi"
 import { ConnectKitProvider } from "connectkit"
 import { config } from "./config/wagmi"
 import { Autenticacion } from "./pages"
+import { Toaster } from "react-hot-toast";
+
 export default function App() {  
   return(    
       <WagmiConfig config={config}>
+          <Toaster position="bottom-center  " />
           <ConnectKitProvider>
           <AppLayout>
             <Autenticacion />
