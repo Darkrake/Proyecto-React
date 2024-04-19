85f7594511fdd76f4684a9d55906108ef83ba7fd
zori13 <zori.gon@mail.com>
2024-04-19 13:19:29 +0200

    Modificar el componente

--- src/pages/GestionUsuarios.jsx
+++ src/pages/GestionUsuarios.jsx
@@ -3,11 +3,11 @@ import { AltaPrestamista, AltaCliente } from "../components"
 export default function GestionUsuarios() {
   return (
     <div>
-      <div className="text-5xl pt-10 font-thin text-center text-green-700">
-        <h2>Gestion de Usuarios</h2>
+      <div className="text-3xl text-bold pt-5 font-thin text-center text-green-700">
+        <h3>Gestion de Usuarios</h3>
         <hr />
       </div>
-      <section className="flex flex-col pt-10 gap-16 items-center">
+      <section className="flex flex-col pt-5 gap-8 items-center">
         <div>
           <AltaPrestamista />
         </div>
